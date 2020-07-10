5b078185c67d75892272daf0630fb41a82788ad3
root <root@localhost.localdomain>
2020-07-10 17:58:54 +0300

    third commit

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 app_name = 'mainsite'
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('/about_page/',views.about_page,name='about_page'),
+    path('/activities/',views.activities,name='activities')
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request,'mainsite/index.html',context)
+
+
+def about_page(request):
+    context = {}
+    return render(request,'mainsite/about.html',context)
+
+def activities(request):
+    context = {}
+    return render(request,'mainsite/activities.html',context)
